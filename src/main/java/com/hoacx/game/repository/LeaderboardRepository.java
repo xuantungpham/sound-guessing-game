b78f80a5fe440c9c428119923ad343312e623859
B21DCCN375 <100017266+Hoku727@users.noreply.github.com>
2025-11-05 06:26:25 +0700

    Update LeaderboardRepository.java

--- src/main/java/com/hoacx/game/repository/LeaderboardRepository.java
+++ src/main/java/com/hoacx/game/repository/LeaderboardRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LeaderboardRepository extends JpaRepository<Leaderboard, Integer> {
-    // Có thể thêm custom query sau nếu cần, ví dụ:
-    // List<Leaderboard> findTop10ByOrderByScoreDesc();
+    
 }
